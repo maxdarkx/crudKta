15e3cbd74ddc988697fd8c6f039cd6c161866a9d
maxdarkx <maxdarkx@gmail.com>
2021-09-16 09:57:01 -0500

    se crea un servicio backend basico para el manejo de una sola lista todo, se verifica el correcto funcionamiento

---
+++ katalist_back/src/main/java/com/jcarlosmaya/katalist_back/TodoController.java
@@ -0,0 +1,38 @@
+package com.jcarlosmaya.katalist_back;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "api")
+public class TodoController {
+
+    @Autowired
+    private TodoService service;
+
+    @GetMapping(value = "/todos")
+    public Iterable<Todo> get(){
+        return service.get();
+    }
+
+    @GetMapping(value = "/todo/{id}")
+    public Todo getById(@PathVariable Long id)
+    {
+        return service.getById(id);
+    }
+
+    @PostMapping (value = "/todo")
+    public Todo save(@RequestBody Todo todo){
+        return service.save(todo);
+    }
+
+    @DeleteMapping(value = "/todo/{id}")
+    public void delete(@PathVariable("id") Long id){
+        service.delete(id);
+    }
+
+    @PutMapping(value = "/todo")
+    public Todo update(@RequestBody Todo todo){
+        return service.update(todo);
+    }
+}
